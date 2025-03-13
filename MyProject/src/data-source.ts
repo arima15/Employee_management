4536ec4dee2918842f46ef281ca03271fa3001d1
cheriemae20 <cheriemaecatubig21@gmail.com>
2025-03-14 07:58:58 +0800

    database

--- MyProject/src/data-source.ts
+++ MyProject/src/data-source.ts
@@ -6,10 +6,10 @@ import { Department } from "./entity/Department"
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
-    port: 3306,
+    port: 3307,
     username: "root",
-    password: "root",
-    database: "eroy",
+    password: "08272003",
+    database: "Cherie",
     synchronize: true,
     logging: false,
     entities: [Employee, Department],
